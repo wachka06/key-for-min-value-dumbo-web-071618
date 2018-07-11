a4c116ad93957b776557d1f749cb7aa9a26a110f
wachka06 <wachka06@gmail.com>
2018-07-11 16:41:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_val = nil
   min_name = nil
   name_hash.each do |name, val|
-    if min_val > val
+    if min_val == nil || min_val > val
       min_val = val
       min_name = name
     end
